f2d4099eb16bc3333a0759b15c05ff58cf41e83b
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-04-19 22:44:56 -0500

    Tratando de solucionar algo

--- Profundizando php/validacionFormularios/guardar.php
+++ Profundizando php/validacionFormularios/guardar.php
@@ -1,10 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hola</title>
+</head>
+<body>
+  
+</body>
 <?php
-
-  if(isset($_POST['titulo']) && isset($_POST['descripcion'])){
+  echo "El valor de titulo es: " . $_POST['titulo'];
+  /*if(isset($_POST['titulo']) && isset($_POST['descripcion'])){
     echo $_POST['titulo'];
     echo $_POST['descripcion'];
   }else{
     echo "Hola mundo!";
-}
-
+  }*/
 ?>
+</html>
