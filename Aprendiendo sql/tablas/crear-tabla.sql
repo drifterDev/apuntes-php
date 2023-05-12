48a89d71df3cc5af8a9e1fd9b7bbcb0c7c8b27aa
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-05-12 18:11:03 -0500

    Cambio?

---
+++ Aprendiendo sql/tablas/crear-tabla.sql
@@ -0,0 +1,32 @@
+/*
+Creación de una tabla en sql
+
+
+
+Tipos de datos en los campos:
+int(numero caracteres)  ENTERO
+integer(numero caracteres)  ENTERO (maximo 4294967295)
+char(numero caracteres)  STRING / ALFANUMERICO
+varchar(numero caracteres)  STRING / ALFANUMERICO
+float(numero cifras, numero decimales)  DECIMAL/COMA FLOTANTE
+date, time, timestrap
+
+
+//STRING GRANDES
+TEXT(65335 caracteres mas o menos)
+MEDIUMTEXT (16 millones mas o menos)
+LONGTEXT (4 billones mas o menos)
+
+
+//ENTEROS GRANDES 
+MEDIUMINT
+BIGINT
+*/
+
+CREATE TABLE usuarios(
+id        int(11),
+nombre    varchar(100),
+apellidos varchar(255),
+email     varchar(100),
+password  varchar(255)
+);
