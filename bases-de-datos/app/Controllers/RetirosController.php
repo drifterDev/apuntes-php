73ecbec60c424179aebdf753b22d671f0e289395
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-26 09:06:40 -0500

    Controllers

---
+++ bases-de-datos/app/Controllers/RetirosController.php
@@ -0,0 +1,70 @@
+<?php
+
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio. 
+
+namespace App\Controller;
+
+class RetirosController
+{
+  /**
+   * Muestra una lista de este recurso
+   */
+  public function index()
+  {
+  }
+
+  /**
+   * Muestra un formulario para crear un nuevo recurso
+   */
+  public function create()
+  {
+  }
+
+  /**
+   * Guarda un nuevo recurso en la base de datos
+   */
+  public function store()
+  {
+  }
+
+  /**
+   * Muestra un único recurso especificado
+   */
+  public function show()
+  {
+  }
+
+  /**
+   * Muestra el formulario para editar un recurso
+   */
+  public function edit()
+  {
+  }
+
+  /**
+   * Actualiza un recurso específico en la base de datos
+   */
+  public function update()
+  {
+  }
+
+  /**
+   * Elimina un recurso específico de la base de datos
+   */
+  public function destroy()
+  {
+  }
+}
+
+
+// index: muestra la lista de todos los recursos.
+// create: muestra un formulario para ingresar un nuevo recurso. (luego manda a llamar al método store).
+// store: registra dentro de la base de datos el nuevo recurso.
+// show: muestra un recurso específico.
+// edit: muestra un formulario para editar un recurso. (luego manda a llamar al método update).
+// update: actualiza el recurso dentro de la base de datos.
+// destroy: elimina un recurso.
