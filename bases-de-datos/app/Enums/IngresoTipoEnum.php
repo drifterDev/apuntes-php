f0550c9c58b9ec7568e8c0c717844c670c2c6f9f
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-26 09:13:14 -0500

    Enums para los tipos tinyint

---
+++ bases-de-datos/app/Enums/IngresoTipoEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+namespace App\Enums;
+
+enum IngresoTipoEnum: int
+{
+  case Retiro = 1;
+  case Compra = 2;
+}
