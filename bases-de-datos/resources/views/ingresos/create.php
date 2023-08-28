cd394670bfa82d2f5f38cbc98084732a07284084
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-28 08:39:39 -0500

    Comenzando a ver como funcionan los frameworks

--- bases-de-datos/resources/views/ingresos/create.php
+++ bases-de-datos/resources/views/ingresos/create.php
@@ -3,37 +3,50 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Agrega un nuevo ingreso</title>
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/create.css">
+  <title>Agrega nuevo ingreso</title>
 </head>
 
 <body>
   <h1>Agrega un nuevo ingreso</h1>
-  <form action="/ingresos" method="post">
-    <label for="metodo_pago">Metodo de pago</label>
-    <select name="metodo_pago" id="metodo_pago">
-      <option value="1" selected>Tarjeta de credito</option>
-      <option value="2">Cuenta de ahorros</option>
-    </select>
+  <main>
+    <form action="/incomes" method="post">
 
-    <label for="tipo"></label>
-    <select name="tipo" id="tipo">
-      <option value="1" selected>Salario</option>
-      <option value="2">Devolucion</option>
-    </select>
-
-    <label for="fecha">Fecha</label>
-    <input type="text" name="fecha" id="fecha">
-
-    <label for="cantidad">Cantidad</label>
-    <input type="number" name="cantidad" id="cantidad">
-
-    <label for="descripcion">Descripcion</label>
-    <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea>
-    <input type="hidden" name="method" value="post">
-    <button type="submit">Guardar</button>
-  </form>
+      <div class="input-group">
+        <label for="metodo_pago">Metodo de pago</label>
+        <select name="metodo_pago" id="metodo_pago">
+          <option value="1" selected>Tarjeta de credito</option>
+          <option value="2">Cuenta de ahorros</option>
+        </select>
+      </div>
+      <div class="input-group">
+        <label for="tipo">Tipo de ingreso</label>
+        <select name="tipo" id="tipo">
+          <option value="1" selected>Salario</option>
+          <option value="2">Devolucion</option>
+        </select>
+      </div>
+      <div class="input-group">
+        <label for="fecha">Fecha</label>
+        <input type="text" name="fecha" id="fecha">
+      </div>
+      <div class="input-group">
+        <label for="cantidad">Cantidad</label>
+        <input type="number" name="cantidad" id="cantidad">
+      </div>
+      <div class="input-group">
+        <label for="descripcion">Descripcion</label>
+        <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea>
+      </div>
+      <input type="hidden" name="method" value="post">
+      <div class="button-container">
+        <button class="button-link" type="submit">Guardar</button>
+      </div>
+    </form>
+  </main>
 </body>
 
 </html>
--- bases-de-datos/resources/views/ingresos/index.php
+++ bases-de-datos/resources/views/ingresos/index.php
@@ -3,17 +3,38 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/index.css">
   <title>Lista de ingresos</title>
 </head>
 
 <body>
   <h1>Lista de ingresos</h1>
-  <ul>
-    <?php foreach ($results as $r) : ?>
-      <li>Te fueron ingresados <?= $r["cantidad"] ?> COP de <?= $r["descripcion"] ?></li>
-    <?php endforeach; ?>
-  </ul>
+  <main>
+    <table>
+      <tr>
+        <th>Método de pago</th>
+        <th>Tipo de ingreso</th>
+        <th>Fecha</th>
+        <th>Cantidad</th>
+        <th>Descripción</th>
+      </tr>
+      <tbody>
+        <?php foreach ($results as $r) : ?>
+          <tr>
+            <td><?= $r["metodo_pago"] ?></td>
+            <td><?= $r["tipo"] ?></td>
+            <td><?= $r["fecha"] ?></td>
+            <td><?= $r["cantidad"] ?></td>
+            <td><?= $r["descripcion"] ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+    <a class="button-link" href="/ingresos/create">Agregar nuevo ingreso</a>
+  </main>
 </body>
 
 </html>
