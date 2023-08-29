a80d1b311f91fb8371e2ae01d799165f0c66b48a
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-29 13:02:53 -0500

    GET y POST

---
+++ api-rest/servicios-rest/router.php
@@ -0,0 +1,26 @@
+<?php
+
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+// Antes:
+// http://localhost:8000?type=books&id=2
+// Ahora:
+// http://localhost:8000/books/2
+$matches = [];
+if (preg_match('/\/([^\/]+)\/([^\/]+)/', $_SERVER["REQUEST_URI"], $matches)) {
+  $_GET['type'] = $matches[1];
+  $_GET['id'] = $matches[2];
+  error_log(print_r($matches, 1));
+  require 'server.php';
+} else if (preg_match('/\/([^\/]+)\/?/', $_SERVER["REQUEST_URI"], $matches)) {
+  $_GET['type'] = $matches[1];
+  error_log(print_r($matches, 1));
+  require 'server.php';
+} else {
+  error_log('No matches');
+  http_response_code(404);
+}
---
+++ api-rest/servicios-rest/server.php
@@ -0,0 +1,73 @@
+<?php
+
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+$books = [
+  1 => [
+    'titulo' => 'El imperio final',
+    'id_author' => 1,
+    'id_genero' => 1,
+  ],
+  2 => [
+    'titulo' => 'El bestiario de Axlin',
+    'id_author' => 2,
+    'id_genero' => 2,
+  ],
+  3 => [
+    'titulo' => 'El hijo de neptuno',
+    'id_author' => 3,
+    'id_genero' => 1,
+  ]
+];
+
+// Definir los recursos disponibles
+$allowedResourceTypes = [
+  'books',
+  'authors',
+  'genres'
+];
+
+// Validación del recurso
+$resourceType = $_GET['type'];
+
+if (!in_array($resourceType, $allowedResourceTypes)) {
+  die;
+}
+
+// Lo siguiente son buenas practicas
+header('Content-type: application/json');
+
+// Obtenemos el id del recurso
+$resourceId = array_key_exists('id', $_GET) ? $_GET['id'] : '';
+
+// Generamos la respuesta dependiendo de la petición
+switch (strtoupper($_SERVER['REQUEST_METHOD'])) {
+  case 'GET':
+    if (empty($resourceId)) {
+      echo json_encode($books);
+    } else if (array_key_exists($resourceId, $books)) {
+      echo json_encode($books[$resourceId]);
+    }
+    break;
+  case 'POST':
+    // para acceder a lo que nos envia el usuario
+    $json = file_get_contents('php://input');
+    $books[] = json_decode($json, true);
+    // echo array_keys($books)[count($books) - 1]; Devuelve el nuevo id
+    echo json_encode($books);
+    break;
+  case 'PUT':
+    # code...
+    break;
+  case 'DELETE':
+    # code...
+    break;
+
+  default:
+    # code...
+    break;
+}
