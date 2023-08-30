98ec5508335b0236d545d929db55184c1753dbc1
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-29 22:09:20 -0500

    Request y organización de carpetas

---
+++ frameworks/app/Http/Request.php
@@ -0,0 +1,62 @@
+<?php
+
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+namespace App\Http;
+
+class Request
+{
+  protected $segments = [];
+  protected $controller;
+  protected $method;
+
+  public function __construct()
+  {
+    $this->segments = explode('/', $_SERVER['REQUEST_URI']);
+    $this->setMethod();
+    $this->setController();
+  }
+
+  public function setController()
+  {
+    $this->controller = empty($this->segments[1])
+      ? 'home'
+      : $this->segments[1];
+  }
+
+  public function setMethod()
+  {
+    $this->method = empty($this->segments[2])
+      ? 'index'
+      : $this->segments[2];
+  }
+
+  public function getController()
+  {
+    // Uppercase
+    $controller = ucfirst($this->controller);
+    return "App\Http\Controllers\\{$controller}Controller";
+  }
+
+  public function getMethod()
+  {
+    return $this->method;
+  }
+
+  public function send()
+  {
+    $controller = $this->getController();
+    $method = $this->getMethod();
+
+    $response = call_user_func([
+      new $controller,
+      $method
+    ]);
+
+    $response->send();
+  }
+}
--- frameworks/public/index.php
+++ frameworks/public/index.php
@@ -1,6 +1,14 @@
 <?php
 
-require __DIR__ . '../vendor/autoload.php';
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
 
-$request = new App\Http\Request;
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+use App\Http\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$request = new Request;
 $request->send();
